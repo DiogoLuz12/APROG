231d5104f31705af7347d773ca4dafae8c0066a4
DiogoLuz12 <x7diogoluz@gmail.com>
2023-05-12 09:16:31 +0100

    Criar a funçao para converter um livro em contentValues

---
+++ app/src/main/java/pt/ipg/mywork/Livro.kt
@@ -0,0 +1,21 @@
+package pt.ipg.mywork
+
+import android.content.ContentValues
+
+data class Livro(
+    var titulo: String,
+    var idCategoria: Int,
+    var isbn: String? = null,
+    var id: Long = -1
+){
+
+    fun toContentValues(): ContentValues{
+        val valores = ContentValues()
+
+        valores.put(TabelaLivros.CAMPO_TITULO, titulo)
+        valores.put(TabelaLivros.CAMPO_ISBN, isbn)
+        valores.put(TabelaLivros.CAMPO_FK_CATEGORIA, idCategoria)
+
+        return valores
+    }
+}
--- app/src/main/java/pt/ipg/mywork/TabelaLivros.kt
+++ app/src/main/java/pt/ipg/mywork/TabelaLivros.kt
@@ -7,10 +7,13 @@ import android.provider.BaseColumns
 class TabelaLivros(db: SQLiteDatabase) : TabelaBD(db, NOME_TABELA){
     companion object{
         const val NOME_TABELA = "livros"
+        const val CAMPO_TITULO = "titulo"
+        const val CAMPO_ISBN = "isbn"
+        const val CAMPO_FK_CATEGORIA ="id_categorias"
     }
 
     override fun cria() {
-        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, titulo TEXT NOT NULL, isbn TEXT, id_categoria INTEGER NOT NULL UNIQUE,    FOREIGN KEY(id_categoria) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT)")
+        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, $CAMPO_TITULO TEXT NOT NULL, $CAMPO_ISBN TEXT, $CAMPO_FK_CATEGORIA INTEGER NOT NULL UNIQUE,    FOREIGN KEY($CAMPO_FK_CATEGORIA) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT)")
     }
 
 
