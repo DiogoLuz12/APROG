8c10bb5140ceb2d0568fdfce976ac0acdf97b0f0
DiogoLuz12 <x7diogoluz@gmail.com>
2023-05-30 09:36:41 +0100

    Criado UriMatcher para validar os endereços do content provider

--- app/src/main/java/pt/ipg/mywork/LivrosContentProvider.kt
+++ app/src/main/java/pt/ipg/mywork/LivrosContentProvider.kt
@@ -2,6 +2,7 @@ package pt.ipg.livros
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 import pt.ipg.mywork.BdLivrosOpenHelper
@@ -205,5 +206,19 @@ class LivrosContentProvider : ContentProvider() {
         selectionArgs: Array<out String>?
     ): Int {
         TODO("Not yet implemented")
+    }
+    companion object {
+        private const val AUTORIDADE = "pt.ipg.livros"
+
+        const val CATEGORIAS = "categorias"
+        const val LIVROS = "livros"
+
+        private const val URI_CATEGORIAS = 100
+        private const val URI_LIVROS = 200
+
+        fun uriMatcher() = UriMatcher(UriMatcher.NO_MATCH).apply {
+            addURI(AUTORIDADE, CATEGORIAS, URI_CATEGORIAS)
+            addURI(AUTORIDADE, LIVROS, URI_LIVROS)
+        }
     }
 }
